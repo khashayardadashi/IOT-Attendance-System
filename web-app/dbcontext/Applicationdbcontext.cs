5811fb9e025eb33711cfd77e88a033a117630657
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2026-02-04 21:57:52 +0330

    Add files via upload

---
+++ web-app/dbcontext/Applicationdbcontext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_App.Tables;
+
+namespace Web_App.DbContext;
+
+public class Applicationdbcontext : Microsoft.EntityFrameworkCore.DbContext
+{
+
+        public Applicationdbcontext(DbContextOptions<Applicationdbcontext> options):base(options){}
+        public DbSet<User> Users { get; set; }
+}
